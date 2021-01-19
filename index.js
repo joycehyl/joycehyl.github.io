8b260e6a607d00e922c9a964fad70046b09e0487
joycehyl <yho@risd.edu>
2021-01-19 10:25:38 +0800

    addeddesktop

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+//alert("just a desktop");
+$("#sticky").draggable();
+$("#diggraphics").draggable();
+$("#projs").draggable();
+$("#texts").draggable();
+$("#amood").draggable();
+$("#instruction").draggable();
+$("#intro").draggable();
+  })
---
+++ stickynotes.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  //alert("something to decorate this white wall huh?");
+$("#stickynotestitle").hover(function(){
+ $("#floatingthoughtsncolorsintro").toggle();
+});
+
+})
