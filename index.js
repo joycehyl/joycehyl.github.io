8441801ef195248ca3200a0a157efafdaaf9e145
joycehyl <yho@risd.edu>
2021-02-09 18:23:28 +0800

    nouveauxspecimens

--- index.js
+++ index.js
@@ -7,4 +7,30 @@ $("#texts").draggable();
 $("#amood").draggable();
 $("#instruction").draggable();
 $("#intro").draggable();
+
+
+  var d = new Date();
+  document.getElementById("time").innerHTML = d.toDateString();
+
+  $("#contact").mouseover(function(){
+   $("#contactinfo").show();
+  });
+  $("#contact").mouseout(function(){
+   $("#contactinfo").hide();
+  });
+
+  $("#settings").mouseover(function(){
+   $("#settingsinfo").show();
+  });
+  $("#settings").mouseout(function(){
+   $("#settingsinfo").hide();
+  });
+
+  $("#time").mouseover(function(){
+   $("#dateinfo").show();
+  });
+  $("#time").mouseout(function(){
+   $("#dateinfo").hide();
+  });
+
   })
